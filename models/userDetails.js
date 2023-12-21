c4df2e0affb704343b734e6c05bfd58fe6a124f8
bapparaj <bapparaj97@gmail.com>
2023-12-21 17:11:54 +0530

    add a database and create models

---
+++ models/userDetails.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const schema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+schema.pre('save', async function(next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const userModel = model('userDetail', schema);
+module.exports = userModel;
